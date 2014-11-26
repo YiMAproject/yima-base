2f96699752c43718e9c9c5586c0102c18e83414f
Payam Naderi <naderi.payam@gmail.com>
2014-11-26 13:07:02 +0330

    Default Exception Listener to send back catched error to app

---
+++ src/yimaBase/Mvc/Listener/SendExceptionListener.php
@@ -0,0 +1,70 @@
+<?php
+namespace yimaBase\Mvc\Listener;
+
+use yimaBase\Mvc\MvcEvent;
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\Mvc\ResponseSender\SendResponseEvent;
+
+class SendExceptionListener implements
+    ListenerAggregateInterface
+{
+
+    /**
+     * @var \Zend\Stdlib\CallbackHandler[]
+     */
+    protected $listeners = array();
+
+    /**
+     * @var SendResponseEvent
+     */
+    protected $event;
+
+    /**
+     * @var EventManagerInterface
+     */
+    protected $eventManager;
+
+    /**
+     * Attach the aggregate to the specified event manager
+     *
+     * @param  EventManagerInterface $events
+     * @return void
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(
+            MvcEvent::EVENT_ERROR
+            , array($this, 'throwException')
+            , -100000
+        );
+    }
+
+    /**
+     * Finally Throw Exception Catched By App.
+     *
+     * @param  MvcEvent $e
+     *
+     * @throws \Exception
+     */
+    public function throwException($e)
+    {
+        if ($e->getError() && $e->getError() instanceof \Exception)
+            throw $e->getError();
+    }
+
+    /**
+     * Detach aggregate listeners from the specified event manager
+     *
+     * @param  EventManagerInterface $events
+     * @return void
+     */
+    public function detach(EventManagerInterface $events)
+    {
+        foreach ($this->listeners as $index => $listener) {
+            if ($events->detach($listener)) {
+                unset($this->listeners[$index]);
+            }
+        }
+    }
+}
---
+++ src/yimaBase/Mvc/MvcEvent.php
@@ -0,0 +1,7 @@
+<?php
+namespace yimaBase\Mvc;
+
+class MvcEvent extends \Zend\Mvc\MvcEvent
+{
+    const EVENT_ERROR      = 'error';
+}
