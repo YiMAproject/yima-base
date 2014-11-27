955d39415768985ebad2c3b072637c5701f20b1e
Payam Naderi <naderi.payam@gmail.com>
2014-11-27 12:22:14 +0330

    handle runtime exceptions

--- src/yimaBase/Mvc/Application.php
+++ src/yimaBase/Mvc/Application.php
@@ -3,5 +3,28 @@ namespace yimaBase\Mvc;
 
 class Application extends \Zend\Mvc\Application
 {
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        try
+        {
+            $result = parent::run();
+        }
+        catch(\Exception $e)
+        {
+            // Set Accrued Exception as MVC Error
+            $this->getMvcEvent()->setError($e);
+            $this->getEventManager()->trigger(MvcEvent::EVENT_ERROR, $this->getMvcEvent());
+            // with default SendExceptionListener
+            // Throw accrued exception so we may don't reach this lines below
+            // ...
+            $this->run();
 
+            $result = false;
+        }
+
+        return $result;
+    }
 }
